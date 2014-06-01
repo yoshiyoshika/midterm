5b944fca961af6c3802f1a458db0cc307442bb72
N.Shimizu <chikoski@gmail.com>
2014-06-01 16:43:36 +0900

    第8問を追加

---
+++ test/test_find_first_even_number.js
@@ -0,0 +1,26 @@
+describe("findFirstEvenNumber: 引数を一つ取る関数findFirstEvenNumberを、次の条件を満たすように定義せよ。", function(){
+
+    context("引数が配列のとき", function(){
+        
+        it("最初の偶数の値を返す", function(){
+            findFirstEvenNumber([1, 2, 3, 4]).should.equal(2);
+            findFirstEvenNumber([2, 1, 3]).should.equal(2);
+            findFirstEvenNumber([1, -2, 2, 3]).should.equal(-2);
+            findFirstEvenNumber([2]).should.equal(2);
+        });
+
+        context("配列の長さが0の場合", function(){
+            it("nullを返す", function(){
+                findFirstEvenNumber([]).should.equal(null);
+            });
+        });
+
+        context("要素が全て奇数の場合", function(){
+            it("nullを返す", function(){
+                findFirstEvenNumber([1, 3, 5, 7, 9]).should.equal(null);
+            });
+        });
+        
+    });
+    
+});
